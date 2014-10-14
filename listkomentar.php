6a5df9ad299a805a3977769165c90e7a7460bfd6
steve1994 <steve.harnadi@gmail.com>
2014-10-14 08:43:12 +0100

    Create listkomentar.php

---
+++ listkomentar.php
@@ -0,0 +1,30 @@
+<?php
+	// VARIABEL (POST DAN GET)
+	$ID_post = $_GET['q1'];
+	$response_text=""; // Variabel penampung string hasil
+	
+	// LAKUKAN PENGISIAN RESPONSE TEXT 
+	// Buat koneksi ke database tubesweb1
+	$connection = mysqli_connect('localhost', "root", "", "tubesweb1");
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " .mysqli_connect_error();
+	}
+	// Baca isi tabel daftar komentar masukkan ke variabel response text
+	$hasil_baca = mysqli_query($connection, "SELECT * FROM daftarkomentar 
+				  WHERE ID_post_terkait='$ID_post' ORDER BY TanggalKomentar desc");
+	while ($row = mysqli_fetch_array($hasil_baca))
+	{	
+		$response_text.= "<li class='art-list-item'>";
+		$response_text.= "	<div class='art-list-item-title-and-time'>";
+		$response_text.= "		<h2 class='art-list-title'><a href='post.html'>".$row['Nama']."</a></h2>";
+		$response_text.= "		<div class='art-list-time'>".$row['TanggalKomentar']."</div>";
+		$response_text.= "	</div>";
+		$response_text.= "	<p>".$row['IsiKomentar']."</p>";
+		$response_text.= "</li>";
+	}
+	// Akhiri transaksi
+	mysqli_close($connection);
+	
+	echo $response_text; // PRINT HASIL RESPONSE_TEXT
+?>
