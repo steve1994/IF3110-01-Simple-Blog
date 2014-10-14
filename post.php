8cb1da8dd3cd8495089677c074c8a789358095b2
steve1994 <steve.harnadi@gmail.com>
2014-10-14 08:35:12 +0100

    Update and rename post.html to post.php

--- post.php
+++ post.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
 <head>
 
@@ -34,22 +33,62 @@
 
 </head>
 
-<body class="default">
+<body class="default" onload="LoadListKomentar()">
 <div class="wrapper">
 
 <nav class="nav">
-    <a style="border:none;" id="logo" href="index.html"><h1>Simple<span>-</span>Blog</h1></a>
+    <a style="border:none;" id="logo" href="index.php"><h1>Simple<span>-</span>Blog</h1></a>
     <ul class="nav-primary">
-        <li><a href="new_post.html">+ Tambah Post</a></li>
+        <li><a href="new_post.php">+ Tambah Post</a></li>
     </ul>
 </nav>
+
+<?php
+	$ID_post = $_GET["q"]; // passing parameter q berisi id post yang diklik dari index.php ke halaman ini
+?>
 
 <article class="art simple post">
     
     <header class="art-header">
         <div class="art-header-inner" style="margin-top: 0px; opacity: 1;">
-            <time class="art-time">15 Juli 2014</time>
-            <h2 class="art-title">Apa itu Simple Blog?</h2>
+            <time class="art-time">
+				<?php
+					// MENAMPILKAN TANGGAL POST
+					// Buat koneksi ke database tubesweb1
+					$connection = mysqli_connect('localhost', "root", "", "tubesweb1");
+					if (mysqli_connect_errno())
+					{
+						echo "Failed to connect to MySQL: " .mysqli_connect_error();
+					}
+
+					$hasil_baca = mysqli_query($connection, "SELECT * FROM daftarpost where ID='$ID_post'");
+					while ($row = mysqli_fetch_array($hasil_baca))
+					{	
+						echo $row['Tanggal'];
+					}
+					// Akhiri transaksi
+					mysqli_close($connection);
+				?>
+			</time>
+            <h2 class="art-title">
+				<?php
+					// MENAMPILKAN JUDUL POST
+					// Buat koneksi ke database tubesweb1
+					$connection = mysqli_connect('localhost', "root", "", "tubesweb1");
+					if (mysqli_connect_errno())
+					{
+						echo "Failed to connect to MySQL: " .mysqli_connect_error();
+					}
+
+					$hasil_baca = mysqli_query($connection, "SELECT * FROM daftarpost where ID='$ID_post'");
+					while ($row = mysqli_fetch_array($hasil_baca))
+					{	
+						echo $row['Judul'];
+					}
+					// Akhiri transaksi
+					mysqli_close($connection);
+				?>
+			</h2>
             <p class="art-subtitle"></p>
         </div>
     </header>
@@ -57,15 +96,34 @@
     <div class="art-body">
         <div class="art-body-inner">
             <hr class="featured-article" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis aliquam minus consequuntur amet nulla eius, neque beatae, nostrum possimus, officiis eaque consectetur. Sequi sunt maiores dolore, illum quidem eos explicabo! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam consequuntur consequatur molestiae saepe sed, incidunt sunt inventore minima voluptatum adipisci hic, est ipsa iste. Nobis, aperiam provident quae. Reprehenderit, iste.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores animi tenetur nam delectus eveniet iste non culpa laborum provident minima numquam excepturi rem commodi, officia accusamus eos voluptates obcaecati. Possimus?</p>
+            <p>
+				<?php
+					// MENAMPILKAN KONTEN POST
+					// Buat koneksi ke database tubesweb1
+					$connection = mysqli_connect('localhost', "root", "", "tubesweb1");
+					if (mysqli_connect_errno())
+					{
+						echo "Failed to connect to MySQL: " .mysqli_connect_error();
+					}
+
+					$hasil_baca = mysqli_query($connection, "SELECT * FROM daftarpost where ID='$ID_post'");
+					while ($row = mysqli_fetch_array($hasil_baca))
+					{	
+						echo $row['IsiPostHTML'];
+					}
+					// Akhiri transaksi
+					mysqli_close($connection);
+				?>
+			</p>
 
             <hr />
             
             <h2>Komentar</h2>
 
             <div id="contact-area">
-                <form method="post" action="#">
+                <form method="get" action="#" name="form_komentar" onsubmit="return false;">
+                    <input type="hidden" name="ID_post" id="ID_post" value="<?php echo $ID_post;?>">
+					
                     <label for="Nama">Nama:</label>
                     <input type="text" name="Nama" id="Nama">
         
@@ -75,27 +133,12 @@
                     <label for="Komentar">Komentar:</label><br>
                     <textarea name="Komentar" rows="20" cols="20" id="Komentar"></textarea>
 
-                    <input type="submit" name="submit" value="Kirim" class="submit-button">
+                    <input type="submit" name="submit" value="Kirim" class="submit-button" onclick="LoadKomentarAJAX()">
                 </form>
             </div>
 
-            <ul class="art-list-body">
-                <li class="art-list-item">
-                    <div class="art-list-item-title-and-time">
-                        <h2 class="art-list-title"><a href="post.html">Jems</a></h2>
-                        <div class="art-list-time">2 menit lalu</div>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis repudiandae quae natus quos alias eos repellendus a obcaecati cupiditate similique quibusdam, atque omnis illum, minus ex dolorem facilis tempora deserunt! &hellip;</p>
-                </li>
-
-                <li class="art-list-item">
-                    <div class="art-list-item-title-and-time">
-                        <h2 class="art-list-title"><a href="post.html">Kave</a></h2>
-                        <div class="art-list-time">1 jam lalu</div>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis repudiandae quae natus quos alias eos repellendus a obcaecati cupiditate similique quibusdam, atque omnis illum, minus ex dolorem facilis tempora deserunt! &hellip;</p>
-                </li>
-            </ul>
+            <ul class="art-list-body" id="ListKomentarPost">
+			</ul>
         </div>
     </div>
 
@@ -122,20 +165,12 @@
 
 </div>
 
-<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+
+<script type="text/javascript" src="assets/js/load_list_komentar.js"></script>
+<script type="text/javascript" src="assets/js/validasi_email.js"></script>
 <script type="text/javascript" src="assets/js/fittext.js"></script>
 <script type="text/javascript" src="assets/js/app.js"></script>
 <script type="text/javascript" src="assets/js/respond.min.js"></script>
-<script type="text/javascript">
-  var ga_ua = '{{! TODO: ADD GOOGLE ANALYTICS UA HERE }}';
-
-  (function(g,h,o,s,t,z){g.GoogleAnalyticsObject=s;g[s]||(g[s]=
-      function(){(g[s].q=g[s].q||[]).push(arguments)});g[s].s=+new Date;
-      t=h.createElement(o);z=h.getElementsByTagName(o)[0];
-      t.src='//www.google-analytics.com/analytics.js';
-      z.parentNode.insertBefore(t,z)}(window,document,'script','ga'));
-      ga('create',ga_ua);ga('send','pageview');
-</script>
 
 </body>
 </html>
