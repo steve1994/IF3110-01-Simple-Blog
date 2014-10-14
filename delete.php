cdffcdaab14fed11b82245d05d4c1a9021e1babe
steve1994 <steve.harnadi@gmail.com>
2014-10-14 08:39:57 +0100

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Deskripsi Blog">
+<meta name="author" content="Judul Blog">
+
+<!-- Twitter Card -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="omfgitsasalmon">
+<meta name="twitter:title" content="Simple Blog">
+<meta name="twitter:description" content="Deskripsi Blog">
+<meta name="twitter:creator" content="Simple Blog">
+<meta name="twitter:image:src" content="{{! TODO: ADD GRAVATAR URL HERE }}">
+
+<meta property="og:type" content="article">
+<meta property="og:title" content="Simple Blog">
+<meta property="og:description" content="Deskripsi Blog">
+<meta property="og:image" content="{{! TODO: ADD GRAVATAR URL HERE }}">
+<meta property="og:site_name" content="Simple Blog">
+
+<link rel="stylesheet" type="text/css" href="assets/css/screen.css" />
+
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+
+</head>
+
+<body class="default">
+<script>
+	
+<?php
+	$ID_post = $_GET['q']; // passing parameter url
+	
+	// Buat koneksi ke phpmyadmin sekaligus database tubesweb1
+	$connection = mysqli_connect('localhost', "root", "", "tubesweb1");
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " .mysqli_connect_error();
+	}
+	
+	// DELETE RECORD DI DATABASE
+	mysqli_query($connection,"DELETE FROM daftarpost WHERE ID='$ID_post'");
+	mysqli_query($connection,"DELETE FROM daftarkomentar WHERE ID='$ID_post'");
+	mysqli_close($connection);
+?>
+</script>
+
+<H2>Post Berhasil Dihapus</H2>
+<a href="index.php">LANJUT KE HALAMAN UTAMA</a>
+</body>
+</html>
+
